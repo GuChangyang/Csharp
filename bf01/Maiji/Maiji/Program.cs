c36a7547bf1bcb81bebd3fd75a20211ea0bf357b
Changyang <cgu5@buffalo.edu>
2015-01-20 11:54:55 -0500

    add bf01 folder

---
+++ bf01/Maiji/Maiji/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maiji
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j;
+            int k;
+            for (i = 0; i <= 20;i++ )
+            {
+                for (j = 0; j <= 33; j++)
+                {
+                    for (k = 0; k <= 300; k++)
+                    {
+                        if ((i + j + k == 100) && (5 * i + 3 * j + k / 3) == 100 && (k % 3 == 0))
+                        {
+                            Console.WriteLine("公鸡个数为：{0}\n母鸡个数为：{1}\n雏鸡个数为：{2} ", i, j, k);
+                        }
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ bf01/TicketPrice/TicketPrice/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketPrice
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("优惠后票价为："+ getprice());
+            Console.ReadLine();
+        }
+       public static string getprice()
+       {
+           Console.WriteLine("请输入您属于哪类参观者：\n1-1.2米以下儿童。\n2-大中小学生。\n3-离退休人员。\n4-老年人。\n5-其它。");
+           int ch =int.Parse(Console.ReadLine());
+           Console.WriteLine("请输入您参观故宫的月份。");
+           int month = int.Parse(Console.ReadLine());
+           switch (ch)
+           { 
+               case 1:
+                   return "0";
+                   break;
+                   
+               case 2:
+                   return "20";
+                   break;
+               case 3:
+                   return "0";
+                   break;
+               case 4 :
+                   switch (month)
+                   { 
+                       case 4:
+                       case 5:
+                       case 6:
+                       case 7:
+                       case 8:
+                       case 9:
+                       case 10:
+                           return "30";
+                           break;
+
+                       default :
+                           return "20";
+                           break;                                             
+                   }
+               case 5:
+                   switch (month){
+               case 4:
+               case 5:
+               case 6:
+               case 7:
+               case 8:
+               case 9:
+               case 10:
+                   return "60";
+                   break;
+
+               default:
+                   return "40";
+                   break;
+
+               }
+               default :
+                   return null;
+                   break;
+           }
+       
+       }
+    }
+}
